cddb0c75a99b3e44f6f7d5e7f3432d71b1fb16f5
dkati <dkatikaridis@gmail.com>
2018-10-30 20:00:06 +0200

    SystemUI: StatusBar: Speed up its animations
    Change-Id: Ie2ff4c0c20d7bcceb193d997decb54113371956e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -317,8 +317,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
             .build();
 
-    public static final int FADE_KEYGUARD_START_DELAY = 100;
-    public static final int FADE_KEYGUARD_DURATION = 300;
+    public static final int FADE_KEYGUARD_START_DELAY = 50;
+    public static final int FADE_KEYGUARD_DURATION = 150;
     public static final int FADE_KEYGUARD_DURATION_PULSING = 96;
 
     /** If true, the system is in the half-boot-to-decryption-screen state.
@@ -1755,7 +1755,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                                 + mBackdropBack.getDrawable());
                     }
                     mBackdropFront.animate()
-                            .setDuration(250)
+                            .setDuration(150)
                             .alpha(0f).withEndAction(mHideBackdropFront);
                 }
             }
@@ -1780,7 +1780,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mBackdrop.animate()
                             .alpha(SRC_MIN_ALPHA)
                             .setInterpolator(Interpolators.ACCELERATE_DECELERATE)
-                            .setDuration(300)
+                            .setDuration(200)
                             .setStartDelay(0)
                             .withEndAction(() -> {
                                 mBackdrop.setVisibility(View.GONE);
@@ -1865,7 +1865,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         flagdbg.append('>');
         Log.d(TAG, flagdbg.toString());
 	*/
-	
+
         if ((diff1 & StatusBarManager.DISABLE_EXPAND) != 0) {
             if ((state1 & StatusBarManager.DISABLE_EXPAND) != 0) {
                 animateCollapsePanels();
@@ -5711,7 +5711,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         checkBarModes();
         notifyUiVisibilityChanged(mSystemUiVisibility);
     }
-	
+
     public NotificationGutsManager getGutsManager() {
         return mGutsManager;
     }
