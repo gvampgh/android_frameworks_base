8a52ad3995f2b5700d1e3f692c81d80bf8d4dd97
dkati <dkatikaridis@gmail.com>
2019-04-23 00:23:18 +0300

    core: Never allow BT location scans
    Change-Id: Ibfe7ff06fd6ca61664f017f950a22229790264a2

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -671,14 +671,6 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
 
     @Override
     public boolean isBleScanAlwaysAvailable() {
-        if (isAirplaneModeOn() && !mEnable) {
-            return false;
-        }
-        try {
-            return Settings.Global.getInt(mContentResolver,
-                    Settings.Global.BLE_SCAN_ALWAYS_AVAILABLE) != 0;
-        } catch (SettingNotFoundException e) {
-        }
         return false;
     }
 
