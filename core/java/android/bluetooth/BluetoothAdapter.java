0a1be78d3273c3df63bf0ca670fa2f93e44213bc
dkati <dkatikaridis@gmail.com>
2019-04-23 00:23:18 +0300

    core: Never allow BT location scans
    Change-Id: Ibfe7ff06fd6ca61664f017f950a22229790264a2

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -1520,12 +1520,7 @@ public final class BluetoothAdapter {
      */
     @SystemApi
     public boolean isBleScanAlwaysAvailable() {
-        try {
-            return mManagerService.isBleScanAlwaysAvailable();
-        } catch (RemoteException e) {
-            Log.e(TAG, "remote expection when calling isBleScanAlwaysAvailable", e);
-            return false;
-        }
+        return false;
     }
 
     /**
