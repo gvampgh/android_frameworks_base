6de9162e71d1791d9934c989d2c184334f54ac4f
gvampgh <beatclub@gmx.net>
2019-08-02 14:03:05 +0200

    Clean up the source
    Change-Id: I7fedc4a218782fb8b7351a1eaa3ada99516e8210

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -424,24 +424,21 @@ public class KeyguardIndicationController {
         }
 
         String batteryInfo = "";
-        boolean showbatteryInfo = true;
-         if (showbatteryInfo) {
-            if (mChargingCurrent > 0) {
-                 batteryInfo = batteryInfo + (mChargingCurrent / 1000) + "mA";
-            }
-            if (mChargingVoltage > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                        String.format("%.1f", (mChargingVoltage / 1000 / 1000)) + "V";
-            }
-            if (mTemperature > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                        mTemperature / 10 + "°C";
-            }
-            if (batteryInfo != "") {
-                batteryInfo = "\n" + batteryInfo;
-            }
-        }
 
+        if (mChargingCurrent > 0) {
+           batteryInfo = batteryInfo + (mChargingCurrent / 1000) + "mA";
+        }
+        if (mChargingVoltage > 0) {
+           batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
+                         String.format("%.1f", (mChargingVoltage / 1000 / 1000)) + "V";
+        }
+        if (mTemperature > 0) {
+           batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
+                         mTemperature / 10 + "°C";
+        }
+        if (batteryInfo != "") {
+           batteryInfo = "\n" + batteryInfo;
+        }
 
         String percentage = NumberFormat.getPercentInstance()
                 .format(mBatteryLevel / 100f);
